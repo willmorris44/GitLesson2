5bc7f0a17e0d716a595036e2cfc1666313b007ba
Will morris <willmorris@Wills-MacBook-Pro.local>
2019-05-03 16:18:17 -0600

    first commit

---
+++ GitLesson2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitLesson2
+//
+//  Created by Will morris on 5/3/19.
+//  Copyright © 2019 devmtn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
